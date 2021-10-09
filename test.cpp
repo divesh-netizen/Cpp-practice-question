0d383f5c47c137e8b4fb3223fc2d128dd6812bbe
divesh-netizen <61093504+divesh-netizen@users.noreply.github.com>
2021-10-09 05:54:28 +0530

    10-09-2021

--- test.cpp
+++ test.cpp
@@ -1,10 +1,24 @@
-#include<bits/stdc++.h>
-#include<iostream>
-#include<vector>
+#include <bits/stdc++.h>
 using namespace std;
-
-int main(){
-    cout<<ceil(5/2)<<endl;;
-    cout<<ceil(4/2);
-    return 0;
+void function1(int num, char *param[]){
+    sort(param, param+num);
+    char *ans = "c";
+    int i=1;
+    while(i<num){
+        if(param[i] != param[i-1])
+            ans = param[i-1];
+    }
+    if(ans == "-1")
+        ans = param[num-1];
+    cout<<ans;
+ }
+int main()
+{
+  int n;
+  cin>>n;
+  char *a[n];
+  for(int i=0; i<n; i++)
+    cin>>a[i];
+  function1(n, a);
+  return 0;
 }
