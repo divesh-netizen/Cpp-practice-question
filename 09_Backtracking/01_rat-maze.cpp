d16abfe80c36d5faa87ee07ca0db52ab3750148d
divesh-netizen <61093504+divesh-netizen@users.noreply.github.com>
2021-10-24 18:52:13 +0530

    last_commit

---
+++ 09_Backtracking/01_rat-maze.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> visited(5, vector<int> (5,0));
+vector<string> result;
+
+class Solution{
+    public:
+    void path(vector<vector<int>> &m, int x, int y, string dir, int n) {
+        if (x == n - 1 and y == n - 1) {
+            result.push_back(dir);
+            return;
+        }
+    
+        if (m[x][y] == 0 or visited[x][y] == 1) return;
+    
+        // DFS
+        visited[x][y] = 1;
+        if (x > 0) path(m, x - 1, y, dir + 'U', n);
+        if (y > 0) path(m, x, y - 1, dir + 'L', n);
+        if (x < n - 1) path(m, x + 1, y, dir + 'D', n);
+        if (y < n - 1) path(m, x, y + 1, dir + 'R', n);
+        visited[x][y] = 0;
+    }
+    
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                visited[i][j] = false;
+            }
+        }
+        result.clear();
+    
+        if (m[0][0] == 0 || m[n - 1][n - 1] == 0) return result;
+    
+        path(m, 0, 0, "", n);
+    
+        sort(result.begin(), result.end());
+        return result;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> m(n, vector<int> (n,0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> m[i][j];
+            }
+        }
+        Solution obj;
+        vector<string> result = obj.findPath(m, n);
+        if (result.size() == 0)
+            cout << -1;
+        else
+            for (int i = 0; i < result.size(); i++) cout << result[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
